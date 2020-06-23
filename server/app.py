eecfa566bef427506dfd7d4823183227c7265e6e
Gaoyagi <aoyagigeorge@gmail.com>
2020-06-23 11:55:25 -0700

    Adding FlasAPI

--- server/app.py
+++ server/app.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
+from flask_api import FlaskAPI
 import bcrypt
 
-app = Flask(__name__)
+app = FlaskAPI(__name__)
 
 app.config['MONGO_DBNAME'] = 'staple'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/staple'
 
 mongo = PyMongo(app)
+users = mongo.db.users    #creates db for users
+apis = mongo              #creates db for api 
 
 @app.route('/')
 def index():
-    #probably dont need this
-    # if 'username' in session:
-    #     return 'You are logged in as ' + session['username']
     return render_template('index.html')
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -23,7 +23,6 @@ def login():
         return render_template('login.html')
 
     #submitting a login request
-    users = mongo.db.users    #get list of all users
     login_user = users.find_one({'name' : request.form['username']})    #check to see if theres a user with the same inputted username
 
     #not sure if we need to the if statement? ask padyn later
@@ -32,15 +31,20 @@ def login():
             #encrypts user inputted password and see if it matches the encrypted password in the document found earlier
             if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
                 session['username'] = request.form['username']
-                return redirect(url_for('profile')) #do we need a profile page? ask padyn later
+                #return redirect(url_for('profile')) #do we need a profile page? ask padyn later
+                return {'user' : login_user}
 
     #if invalid login credentials
-    return 'Invalid username/password combination'
+    return None
     #ideally it says invalid login and then directs you back to the login page
-    #return render_template('login.html')
+    #return redirect(request.url)       #back to the login page
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST', 'GET'])
 def register():
+    #return login page
+    if request.method == "GET":
+        return render_template('register.html')
+
     #submitting a new user document
     if request.method == 'POST':
         users = mongo.db.users      #get list of all users
@@ -63,13 +67,16 @@ def register():
 # def profile():
 #     return render_template('profile.html', name=users.username)
 
-# @app.route('/view/api_id')
+# @app.route('/view/<api_id>')
 # def view():
 #     return render_template('view.html')
 
-# @app.route('/edit/api_id')
+# @app.route('/edit/<api_id>/<user_id>')
 # def edit():
+#    if session.get("username", None) is not None:
 #     return render_template('edit.html')
+#   else:
+#     return redirect(url_for('login'))
 
 #logout
 @app.route('/logout')
